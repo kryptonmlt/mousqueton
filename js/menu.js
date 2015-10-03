a30b5879a6caf5d6ee8a4ef747ba77f6b17716ef
David Creigh <davidcreigh@hotmail.co.uk>
2015-10-03 15:14:56 +0100

    Team assignment test

--- js/menu.js
+++ js/menu.js
@@ -5,15 +5,9 @@ function showShips()
     console.log("Using " + shipCount + " Ships");
 }
 
-var assignTeams()
-{
-    
-    
-}
-
-var TEAMS = {
-    TEAM_A = 0,
-    TEAM_B = 1,
-    TEAM_C = 2,
-    TEAM_D = 3
+var theTeams = {
+    TEAM_A : 0,
+    TEAM_B : 1,
+    TEAM_C : 2,
+    TEAM_D : 3
 }
